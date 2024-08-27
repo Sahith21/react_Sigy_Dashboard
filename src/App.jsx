ad20955aff671ef37bed21508f28e22a9303a3e5
Sahith21 <kesaboinasahith@gmail.com>
2024-08-27 20:45:29 +0530

    modified notfound

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Landingpage from './vendor dashboard/pages/Landingpage'
 import {Routes,Route} from 'react-router-dom'
 import "./App.css"
-import Notfound from './vendor dashboard/components/notfound'
+import Notfound from './vendor dashboard/components/Notfound'
 
 
 const App = () => {
